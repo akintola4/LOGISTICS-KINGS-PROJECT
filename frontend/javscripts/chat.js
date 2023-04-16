fbfb56031e8528c48220f0f65cf1adaaf464dfdf
akintola4 <akintope830@gmail.com>
2023-04-16 15:55:00 +0100

    fixed all issues

--- frontend/javscripts/chat.js
+++ frontend/javscripts/chat.js
@@ -84,3 +84,30 @@ function addAdminChat() {
 
 }
 
+
+function addRiderChat() {
+    let chatInput = document.getElementById("myTextarea").value;
+    console.log(chatInput)
+    let div = document.createElement('div')
+    
+    div.classList.add("user", "d-flex", "flex-row", "justify-content-end", "mb-4")
+    let para = document.createElement('p')
+    para.classList.add("bg-light", "row", "rounded-4", "p-3", "col-lg-6")
+
+    let label = document.createElement('label')
+    label.classList.add("userName")
+    label.innerText = "RIDER TOSIN"
+    let i = document.createElement('i')
+    i.classList.add("fa-regular", "fa-user", "p-2")
+    let text = document.createTextNode(chatInput)
+    label.appendChild(i)
+    para.appendChild(label)
+    para.appendChild(text)
+
+
+
+    div.appendChild(para)
+
+    document.getElementById("chats").appendChild(div)
+
+}
